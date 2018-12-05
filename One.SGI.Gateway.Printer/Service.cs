4848d1980e41fdb6eccdcc456f64e65d95d8907d
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2018-12-05 13:13:28 -0300

    Init

---
+++ One.SGI.Gateway.Printer/Service.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Firebase.Database;
+using System.Reactive.Linq;
+using Firebase.Database.Query;
+
+
+namespace One.SGI.Gateway.Printer
+{
+    class Service
+    {
+        public static Service Start(string URL)
+        {
+            return new Service(URL);
+        }
+
+        private Service(string URL)
+        {
+            var client = new FirebaseClient("https://one-sgh.firebaseio.com/");
+            var subs = client
+                .Child("gateway")
+                .AsObservable<GatewayType>()
+                .Where(f => !string.IsNullOrEmpty(f.Key))
+
+
+        }
+    }
+
+    class GatewayType
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ One.SGI.Gateway/Service.cs
@@ -0,0 +1,37 @@
+﻿using Firebase.Database;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One.SGI.Gateway
+{
+    public class Service
+    {
+        private static readonly LiteDatabase localDB = new LiteDatabase(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "One", "SGI", "Gateway", "data"));
+        private static readonly LiteCollection<Model.Gateway> localDB_gateway_col = localDB.GetCollection<Model.Gateway>();
+
+        private readonly FirebaseClient cliente;
+        private readonly Model.Gateway current = localDB_gateway_col.FindAll().FirstOrDefault();
+
+
+        private Service(string URL)
+        {
+            cliente = new FirebaseClient(URL);
+
+            if (current == default(Model.Gateway))
+            {
+                localDB_gateway_col.Insert(new Model.Gateway());
+                current = localDB_gateway_col.FindAll().FirstOrDefault();
+            }
+        }
+
+        public static Service Start()
+        {
+            return new Service("https://one-sgh.firebaseio.com/");
+        }
+
+    }
+}
